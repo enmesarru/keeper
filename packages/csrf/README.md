a704c3be3a21f4573a6d5681d118783d308ded6d
enmesarru <fkurutass@gmail.com>
2024-11-20 19:41:00 +0300

    add tests and rename package

--- packages/csrf/README.md
+++ packages/csrf/README.md
@@ -3,3 +3,8 @@
 <p align="center">Cross-Site Request Forgery</p>
 
 Provides cross-site request forgery (CSRF) protection primitives
+
+<p align="center">
+    <a href="http://npmjs.com/package/keeper"><img alt="📦 npm version" src="https://img.shields.io/npm/v/@keeper/csrf-core?color=21bb42&label=%F0%9F%93%A6%20npm" /></a>
+    <img alt="💪 TypeScript: Strict" src="https://img.shields.io/badge/%F0%9F%92%AA_typescript-strict-21bb42.svg" />
+</p>
--- packages/csrf/src/csrf.test.ts
+++ packages/csrf/src/csrf.test.ts
@@ -1,19 +1,35 @@
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 import { createCsrfKeeper } from "./csrf.js";
 import crypto from "node:crypto";
 
 describe("Signed Double-Submit Cookie", () => {
-	const csrf = createCsrfKeeper({
-		secret: crypto.randomUUID(),
+	it("throws error when secret is not passed at runtime", () => {
+		expect(() =>
+			createCsrfKeeper({
+				secret: undefined as unknown as string,
+			}),
+		).toThrowError(/^Secret key is required.$/);
+
+		expect(() =>
+			createCsrfKeeper({
+				secret: null as unknown as string,
+			}),
+		).toThrowError(/^Secret key is required.$/);
 	});
 
 	it("should generate a random token", () => {
+		const csrf = createCsrfKeeper({
+			secret: crypto.randomUUID(),
+		});
 		const randomValue = csrf.randomToken();
 		expect(randomValue).toHaveLength(64);
 		expect(randomValue).toMatch(/^[0-9a-f]+$/);
 	});
 
 	it("should generate a csrf token with payload", () => {
+		const csrf = createCsrfKeeper({
+			secret: crypto.randomUUID(),
+		});
 		const sessionId = crypto.randomUUID();
 		const randomValue = csrf.randomToken();
 		const csrfToken = csrf.create(randomValue, sessionId);
@@ -24,6 +40,9 @@ describe("Signed Double-Submit Cookie", () => {
 	});
 
 	it("should verify a csrf token", () => {
+		const csrf = createCsrfKeeper({
+			secret: crypto.randomUUID(),
+		});
 		const sessionId = crypto.randomUUID();
 		const randomValue = csrf.randomToken();
 		const csrfToken = csrf.create(randomValue, sessionId);
